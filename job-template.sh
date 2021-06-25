62eee3783ebeb4f06b72c9b60f9e5822a60c648f
Sten Delos <sten@mpa-garching.mpg.de>
2021-06-25 11:39:08 +0200

    mpiexec

--- job-template.sh
+++ job-template.sh
@@ -13,4 +13,5 @@ echo "Running on $SLURM_NPROCS processors."
 echo "Current working directory is `pwd`"
 echo
 
-srun -N $SLURM_NNODES -n $SLURM_NPROCS -c 1 --cpu_bind=cores ~/gadget4/Gadget4 param.txt
+#srun -N $SLURM_NNODES -n $SLURM_NPROCS -c 1 --cpu_bind=cores ~/gadget4/Gadget4 param.txt
+mpiexec -np $SLURM_NPROCS ~/gadget4/Gadget4 param.txt
