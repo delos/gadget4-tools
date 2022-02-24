613b5870fee2142a768981ea833b8bad9f9d5875
Sten Delos <stendelos@gmail.com>
2022-02-24 16:57:01 +0100

    get mean density too

--- src/power_of_shell_corrected.c
+++ src/power_of_shell_corrected.c
@@ -9,7 +9,7 @@
 
 static ptrdiff_t n, n3, npad, n3pad, n1d;
 static float *rho, *shot;
-static double *k, *pk, R, T, pkshot;
+static double *k, *pk, R, T, pkshot, rhomean;
 
 double sinc(double x) {
   if(x > 0.1) return sin(x)/x;
@@ -154,6 +154,7 @@ int save(char *filename) {
   FILE *fp = fopen(filename,"w");
   fprintf(fp,"# R=%lg\n# T=%lg\n",R,T);
   fprintf(fp,"# %le\n",pkshot);
+  fprintf(fp,"# %lg\n",rhomean);
   for(int i=0; i<n1d; i++) {
     fprintf(fp,"%le %le\n",k[i],pk[i]);
   }
@@ -171,6 +172,8 @@ int clean() {
 
 int window() {
   printf("windowing density field between %lg and %lg\n",R-T,R+T);
+  double wsum = 0.;
+  double rhosum = 0.;
   for(ptrdiff_t x = 0; x < n; x++) {
     double x_ = (x + .5)/n - .5;
     for(ptrdiff_t y = 0; y < n; y++) {
@@ -180,9 +183,12 @@ int window() {
         double r = sqrt(x_*x_+y_*y_+z_*z_);
         double w = windowfun(r);
         rho[z+npad*(y+n*x)] *= w;
+        rhosum += rho[z+npad*(y+n*x)];
+        wsum += w;
       }
     }
   }
+  rhomean = rhosum / wsum;
   return 0;
 }
 
