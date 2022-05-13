90c83f4627201939312a83d83c4167bbf475cc16
Sten Delos <sten@mpa-garching.mpg.de>
2022-05-13 09:51:15 +0200

    variable projection depth

--- write_2d_from_binary.py
+++ write_2d_from_binary.py
@@ -8,7 +8,7 @@ cmap = cm.viridis
 def run(argv):
   
   if len(argv) < 2:
-    print('python script.py <filename> [projection axis=2] [project square=0]')
+    print('python script.py <filename> [projection axis=2] [project square=0] [depth fraction=1]')
     return 1
   
   filename = argv[1]
@@ -19,11 +19,22 @@ def run(argv):
   if len(argv) > 3: sq = int(argv[3])
   else: sq = 0
 
+  if len(argv) > 4: depth = float(argv[4])
+  else: depth = 1.
+
   dsize = os.stat(filename).st_size//4
   GridSize = int(dsize**(1./3)+.5)
   with open(filename,'rb') as f:
     delta = np.fromfile(f,count=-1,dtype=np.float32)
   delta.shape = (GridSize,GridSize,GridSize)
+
+  depth_slice = slice( int(np.floor((0.5-0.5*depth)*GridSize)), int(np.ceil((0.5+0.5*depth)*GridSize)) )
+  
+  full_slice = [slice(None,None)]*3
+  full_slice[axis] = depth_slice
+  delta = delta[tuple(full_slice)]
+
+  print('project field of shape ' + str(delta.shape))
 
   if sq:
     delta = np.sqrt(np.mean(delta**2,axis=axis))
@@ -32,6 +43,8 @@ def run(argv):
 
   if axis == 1:
     delta = delta.T # maintain parity
+
+  print('final shape ' + str(delta.shape))
 
   outname = filename + '.%d.2d'%axis
   delta.tofile(outname)
