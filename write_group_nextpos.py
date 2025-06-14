c681edba7cb8b9efa8fed868d27a8c748d2faa7a
Sten Delos <mdelos@carnegiescience.edu>
2025-06-13 23:35:24 -0700

    only 1 snap

--- write_group_nextpos.py
+++ write_group_nextpos.py
@@ -5,43 +5,36 @@ from snapshot_functions import read_subhalos, fileprefix_subhalo, particles_by_I
 def run(argv):
   
   if len(argv) < 2:
-    print('python script.py <ss start>')
+    print('python script.py <ss #>')
     return 1
   
   ss0 = int(argv[1])
 
   ss = ss0
-  while True:
-    if ss > ss0:
-      try:
-        X = np.zeros((np.shape(bins)[0]-1,3))
-        V = np.zeros((np.shape(bins)[0]-1,3))
-        M = np.zeros(np.shape(bins)[0]-1)
-        for typ in range(6):
-          x,v,m, header = particles_by_ID(fileprefix_snapshot%(ss,ss),IDs[typ],opts={'pos':True,'vel':True,'mass':True})
-          indices = np.arange(m.size)
-          for d in range(3):
-            X[:,d] += np.histogram(indices,bins=bins[:,typ],weights=m*x[:,d])[0]
-            V[:,d] += np.histogram(indices,bins=bins[:,typ],weights=m*v[:,d])[0]
-          M += np.histogram(indices,bins=bins[:,typ],weights=m)[0]
-        X /= M[:,None]
-        V /= M[:,None]
-      except Exception as e:
-        print(e)
-        break
-      np.savez('group_nextpos_%03d.npz'%(ss-1),x=X,v=V)
     
-    try:
-      length, header = read_subhalos(fileprefix_subhalo%(ss,ss),opts={},group_opts={'lentype':True,},)
-      bins = np.concatenate((np.zeros(6,dtype=int)[None],np.cumsum(length,axis=0)))
-      IDs = []
-      for typ in range(6):
-        ID, header = read_particles_filter(fileprefix_snapshot%(ss,ss),type_list=[typ],opts={'ID':True},part_range=(bins[0],bins[-1]))
-        IDs += [ID]
-    except Exception as e:
-      print(e)
-      break
-    ss += 1
+  length, header = read_subhalos(fileprefix_subhalo%(ss,ss),opts={},group_opts={'lentype':True,},)
+  bins = np.concatenate((np.zeros(6,dtype=int)[None],np.cumsum(length,axis=0)))
+  IDs = []
+  for typ in range(6):
+    ID, header = read_particles_filter(fileprefix_snapshot%(ss,ss),type_list=[typ],opts={'ID':True},part_range=(bins[0],bins[-1]))
+    IDs += [ID]
+  print(IDs)
+  ss = ss0 + 1
+
+  X = np.zeros((np.shape(bins)[0]-1,3))
+  V = np.zeros((np.shape(bins)[0]-1,3))
+  M = np.zeros(np.shape(bins)[0]-1)
+  for typ in range(6):
+    x,v,m, header = particles_by_ID(fileprefix_snapshot%(ss,ss),IDs[typ],opts={'pos':True,'vel':True,'mass':True})
+    indices = np.arange(m.size)
+    for d in range(3):
+      X[:,d] += np.histogram(indices,bins=bins[:,typ],weights=m*x[:,d])[0]
+      V[:,d] += np.histogram(indices,bins=bins[:,typ],weights=m*v[:,d])[0]
+    M += np.histogram(indices,bins=bins[:,typ],weights=m)[0]
+  X /= M[:,None]
+  V /= M[:,None]
+
+  np.savez('group_nextpos_%03d.npz'%ss0,x=X,v=V)
 
 if __name__ == '__main__':
   from sys import argv
--- write_group_nextpos_mostbound.py
+++ write_group_nextpos_mostbound.py
@@ -5,28 +5,21 @@ from snapshot_functions import subhalo_desc, read_subhalos, fileprefix_subhalo, 
 def run(argv):
   
   if len(argv) < 2:
-    print('python script.py <ss start>')
+    print('python script.py <ss #>')
     return 1
   
   ss0 = int(argv[1])
 
   ss = ss0
-  while True:
-    if ss > ss0:
-      try:
-        x, header = particles_by_ID(fileprefix_snapshot%(ss,ss),grp_IDmb,opts={'pos':True,})
-      except Exception as e:
-        print(e)
-        break
-      np.save('group_nextpos_%03d.npy'%(ss-1),x)
     
-    try:
-      sub_IDmb, grp_sub1, header = read_subhalos(fileprefix_subhalo%(ss,ss),opts={'mostbound':True},group_opts={'firstsub':True,},)
-    except Exception as e:
-      print(e)
-      break
-    grp_IDmb = sub_IDmb[grp_sub1]
-    ss += 1
+  sub_IDmb, grp_sub1, header = read_subhalos(fileprefix_subhalo%(ss,ss),opts={'mostbound':True},group_opts={'firstsub':True,},)
+  grp_IDmb = sub_IDmb[grp_sub1]
+  
+  ss = ss0 + 1
+  
+  x, header = particles_by_ID(fileprefix_snapshot%(ss,ss),grp_IDmb,opts={'pos':True,})
+
+  np.save('group_nextpos_%03d.npy'%ss0,x)
 
 if __name__ == '__main__':
   from sys import argv
